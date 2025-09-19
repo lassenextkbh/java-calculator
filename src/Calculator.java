230f676c659cf7a917a6be56379000dfec507d8f
lassenextkbh <lass365f@edu.next.kbh.dk>
2025-09-19 20:40:06 +0200

    init

---
+++ src/Calculator.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Calculator {
+    // Declare the width and height of the window.
+    int windowWidth = 360;
+    int windowHeight = 540;
+
+    // Declare the colors used in the project
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
+
+    // Declare the values for all the buttons
+    String[] buttonValues = {
+        "AC", "+/-", "%", "÷",
+        "7", "8", "9", "×",
+        "4", "5", "6", "-",
+        "1", "2", "3", "+" ,
+        "0", ".", "√", "="
+    };
+    String[] rightSymbols = {"÷","×", "-", "+", "="}
+    String[] topSymbols = {"AC", "+/-", "%"}
+
+    // Declare window and components
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    Calculator() {
+        // General window settings
+        frame.setVisible(true);
+        frame.setSize(windowWidth,windowHeight);
+        frame.setLocationRelativeTo(null); // Center the window on the screen
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close when the window "close" button is pressed
+        frame.setLayout(new BorderLayout()); // Set up layout for the window
+
+        // Styling for label
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white); // Text color: white
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT); // Make the text align to the right
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        // Styling for panel
+        displayPanel.setLayout(new BorderLayout()); // Set up the layout of the panel
+        displayPanel.add(displayLabel); // Put the text label inside the panel
+
+        frame.add(displayPanel, BorderLayout.NORTH); // Put the panel inside the window (aligned to north/top)
+    }
+}
